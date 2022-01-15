a09723d5f40ead6c05236a77514dcae2c6f50541
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-01-15 14:12:19 -0500

    added start button function

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var startButton = document.getElementById('start-btn')
+var questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    
+}
+
+function setNextQuestion()  {
+
+}
+
+function selectAnswer() {
+
+}
+
+
