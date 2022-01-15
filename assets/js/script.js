2c73e3039c28a8339f0040a721e7e7a789bd066e
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-01-15 15:26:09 -0500

    adding questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,107 @@
 var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
+
+let shuffledQuestions, currentQuestionIndex
+
+
+// will listen for clicks on startGame and nextQuestion buttons
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
+// begins game prompting to first question
 function startGame() {
     console.log('Started')
-    
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
+// shuffles the order of questions and displays next question
 function setNextQuestion()  {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
 
-function selectAnswer() {
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 
 }
 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 
+// after answer is selected will restart game on final question
+function selectAnswer(event) {
+    var selectedButton = event.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// quiz questions with true/false values
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: 'head', correct: true },
+            {text: '<div>', correct: false },
+            {text: '<style>', correct: false}
+        ]
+
+    },
+    {
+        question: "What is the correct syntax for referring to an externa script called xxx.js?",
+        answers: [
+            {text: '<script name="xxx.js"', correct: false },
+            {text: '<script href="xxx.js"', correct: false },
+            {text: '<script src="xxx.js"', correct: true}
+        ]
+    }
+]
